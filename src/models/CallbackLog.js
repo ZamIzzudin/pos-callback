56a4e633376e5acba8ff3814cd65f5e81b587638
azzam <azzam.nutech@gmail.com>
2026-02-16 13:23:28 +0700

    enhance:(change model logs data structure type)

--- src/models/CallbackLog.js
+++ src/models/CallbackLog.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const callbackLogSchema = new mongoose.Schema({
   endpoint: { type: String, required: true },
   method: { type: String, required: true },
-  headers: { type: Object, default: {} },
-  body: { type: Object, default: {} },
-  query: { type: Object, default: {} },
-  ip: { type: String },
+  headers: { type: mongoose.Schema.Types.Mixed, default: {} },
+  body: { type: mongoose.Schema.Types.Mixed, default: {} },
+  query: { type: mongoose.Schema.Types.Mixed, default: {} },
+  ip: { type: String, default: '' },
   hitAt: { type: Date, default: Date.now }
 });
 
